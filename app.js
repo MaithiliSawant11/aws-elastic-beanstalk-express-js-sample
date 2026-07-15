0bb9fee0be6a3190b70e36e6572ffa5d1e381895
MaithiliSawant11 <maithilis350@gmail.com>
2026-07-15 14:56:30 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Aaj aagye wapis mat aana'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
